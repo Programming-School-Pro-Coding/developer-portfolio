9b30d632a3d66235f795a95f3660d578bc8afade
Programming-School <mohamedehabam99@hotmail.com>
2022-05-17 18:52:52 +0200

    Code

--- src/components/Blog/SingleBlog/SingleBlog.js
+++ src/components/Blog/SingleBlog/SingleBlog.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Fade from 'react-reveal/Fade';
 
 import placeholder from '../../../assets/png/placeholder.png';
-import './SingleBlog.css';
+import './SingleBlog.scss';
 
-function SingleBlog({ theme, title, desc, date, image, url, id, source }) {
+function SingleBlog({ theme, title, desc, date, image, url, id }) {
     return (
       <Fade bottom>
         <a
@@ -16,16 +16,23 @@ function SingleBlog({ theme, title, desc, date, image, url, id, source }) {
           style={{ backgroundColor: theme.primary400 }}
         >
           <div
-            className="singleBlog--image"
+            className="singleBlog__image"
             style={{ backgroundColor: theme.secondary }}
           >
             <img src={image ? image : placeholder} alt={title} />
           </div>
-          <div className="singleBlog--body">
+          <div className="singleBlog__body">
             <p style={{ color: theme.tertiary }}>{date}</p>
+            <p style={{ color: theme.tertiary }}>
+              by{" "}
+              <a href="https://github.com/Programming-School-Pro-Coding" target="_blank" rel="noreferrer">
+                Programming School
+              </a>
+            </p>
             <h3 style={{ color: theme.MyColor }}>{title}</h3>
             <h6 style={{ color: theme.MyColor }}>{desc}</h6>
           </div>
+          <div className="singleBlog__categories"></div>
         </a>
       </Fade>
     );
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,7 +3,7 @@ import two from '../assets/svg/projects/two.svg';
 import three from '../assets/svg/projects/three.svg';
 import four from '../assets/svg/projects/four.svg';
 import five from '../assets/svg/projects/five.svg';
-// import six from '../assets/svg/projects/six.svg'
+import six from '../assets/svg/projects/six.svg'
 // import seven from '../assets/svg/projects/seven.svg'
 // import eight from '../assets/svg/projects/eight.svg'
 
@@ -14,7 +14,7 @@ export const projectsData = [
     projectName: "Portfolio",
     projectDesc: "Full responsive Portfolio",
     tags: ["React", "TailwindCSS", "Next JS"],
-    code: "https://github.com/Programing-School/my-portfolio-with-next",
+    code: "https://github.com/Programming-School-Pro-Coding/my-portfolio-with-next",
     demo: "https://my-portfolio-jade-pi.vercel.app/",
     image: one,
   },
@@ -23,8 +23,8 @@ export const projectsData = [
     projectName: "My Blog",
     projectDesc: "It is A Blog with A Dashboard, It looks very nice",
     tags: ["Next", "React", "Tailwind CSS", "GraphQL", "GraphCMS"],
-    code: "https://github.com/Programing-School/Programing-School-Blog",
-    demo: "https://programing-school-blog.vercel.app/",
+    code: "https://github.com/Programming-School-Pro-Coding/Programming-School-Pro-Coding-Blog",
+    demo: "https://Programming-School-Pro-Coding-blog.vercel.app/",
     image: two,
   },
   {
@@ -32,8 +32,8 @@ export const projectsData = [
     projectName: "Sample-Personal-Website",
     projectDesc: "Full responsive Sample Personal Website",
     tags: ["Html", "CSS", "JavaScript"],
-    demo: "https://programing-school.github.io/Sample-Personal-Website/",
-    code: "https://github.com/Programing-School/Sample-Personal-Website.git",
+    demo: "https://Programming-School-Pro-Coding.github.io/Sample-Personal-Website/",
+    code: "https://github.com/Programming-School-Pro-Coding/Sample-Personal-Website.git",
     image: three,
   },
   {
@@ -41,8 +41,8 @@ export const projectsData = [
     projectName: "Rock-Paper-Scissors-game",
     projectDesc: "Full responsive Rock-Paper Scissors Game",
     tags: ["Html", "CSS", "JavaScript"],
-    demo: "https://programing-school.github.io/Rock-Paper-scissors-game/",
-    code: "https://github.com/Programing-School/Rock-Paper-scissors-game.git",
+    demo: "https://Programming-School-Pro-Coding.github.io/Rock-Paper-scissors-game/",
+    code: "https://github.com/Programming-School-Pro-Coding/Rock-Paper-scissors-game.git",
     image: four,
   },
   {
@@ -51,8 +51,26 @@ export const projectsData = [
     projectDesc: "Fully Back-End Project with Next JS",
     tags: ["JavaScript", "Next JS", "Mongo DB"],
     demo: "https://meetup-next-js-project.vercel.app/",
-    code: "https://github.com/Programing-School/Meetup-Next-JS-Project",
+    code: "https://github.com/Programming-School-Pro-Coding/Meetup-Next-JS-Project",
     image: five,
+  },
+  {
+    id: 6,
+    projectName: "GitHub Finder",
+    projectDesc: "Fully responsible project using React JS",
+    tags: ["TypeScript", "React JS", "Styled Components"],
+    demo: "https://github-finder-programming-school.vercel.app/",
+    code: "https://github.com/Programming-School-Pro-Coding/github-finder",
+    image: six,
+  },
+  {
+    id: 7,
+    projectName: "Interactive Comments Section",
+    projectDesc: "Fully responsible project using React JS",
+    tags: ["JavaScript", "React JS", "SCSS", "LocalStorage"],
+    demo: "https://interactive-comments-section-liard.vercel.app/",
+    code: "https://github.com/Programming-School-Pro-Coding/interactive-comments-section",
+    image: six,
   },
 ];
 
