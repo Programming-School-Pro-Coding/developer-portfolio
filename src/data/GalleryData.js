8815772689f179d2613e9c374637f669d0e077b9
Programming-School <mohamedehabam99@hotmail.com>
2022-05-23 14:05:29 +0200

    Add Some Gallery Data

--- src/data/GalleryData.js
+++ src/data/GalleryData.js
@@ -1,6 +1,13 @@
 import JavaScript from "../assets/png/Gallery/JavaScript.jpg";
 import YT from "../assets/png/Gallery/YT.png";
 import Banner from "../assets/png/Gallery/Banner.png";
+import YouTube from '../assets/png/Gallery/YouTube Channels.png';
+import Introduce from '../assets/png/Gallery/Frame 1.png';
+import Visual from '../assets/png/Gallery/Frame 1 (3).png';
+import Git_and_GitHub from '../assets/png/Gallery/Git & github post.png';
+import Windows_11 from "../assets/png/Gallery/How to Try out Windows 11 with a Virtual Machine.png";
+import Banner_2 from "../assets/png/Gallery/YouTube.png";
+import Gamers from "../assets/png/Gallery/RePublic Of Gamers.png";
 
 export const GalleryData = [
   {
@@ -16,9 +23,51 @@ export const GalleryData = [
     image: Banner,
   },
   {
-    id: 4,
+    id: 3,
     Title: "Figma Design",
     subTitle: "Top JavaScript Projects Design",
     image: JavaScript,
+  },
+  {
+    id: 4,
+    Title: "Figma Design",
+    subTitle: "Top YouTube Channels",
+    image: YouTube,
+  },
+  {
+    id: 5,
+    Title: "Figma Design",
+    subTitle: "Introduce myself",
+    image: Introduce,
+  },
+  {
+    id: 6,
+    Title: "Figma Design",
+    subTitle: "What's New in Visual Studio for 2022",
+    image: Visual,
+  },
+  {
+    id: 7,
+    Title: "Figma Design",
+    subTitle: "Git & GitHub",
+    image: Git_and_GitHub,
+  },
+  {
+    id: 8,
+    Title: "Figma Design",
+    subTitle: "How to Try out Windows 11 with a Virtual Machine",
+    image: Windows_11,
+  },
+  {
+    id: 9,
+    Title: "Figma Design",
+    subTitle: "Social Media Banner",
+    image: Banner_2,
+  },
+  {
+    id: 10,
+    Title: "Figma Design",
+    subTitle: "RePublic of Gamers",
+    image: Gamers,
   },
 ];
